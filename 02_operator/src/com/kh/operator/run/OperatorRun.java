a925eefde1fb6134026b828195f0dac499dbbefc
user1 <user1@user1-PC>
2020-10-08 16:48:48 +0900

    added operator

---
+++ 02_operator/src/com/kh/operator/run/OperatorRun.java
@@ -0,0 +1,12 @@
+package com.kh.operator.run;
+
+import com.kh.operator.service.A_SingleOperator;
+
+public class OperatorRun {
+	public static void main(String[] args) {
+		A_SingleOperator aso = new A_SingleOperator();
+		// aso.logicalNegationOp();
+		//aso.increaseOp();
+		aso.practice1();
+	}
+}
---
+++ 02_operator/src/com/kh/operator/service/A_SingleOperator.java
@@ -0,0 +1,54 @@
+package com.kh.operator.service;
+
+public class A_SingleOperator {
+	public void logicalNegationOp() {
+		// 논리 부정 연산자 : !
+		// 논리값(true / false)를 반대로 바꿔주는 연산자
+		boolean isTrue = true;
+		
+		System.out.println("isTrue : " + isTrue);
+		System.out.println("!isTrue : " + !isTrue);
+	}
+	
+	public void increaseOp() {
+		// 증가 연산자 : ++
+		// 피연산자를 1씩 증가 시키는 연산자
+		
+		// ++(피연산자) : 전위 증가 연산자
+		// 먼저 피연산자를 증가시킨 후 다음연산 진행
+		
+		// (피연산자)++ : 후위 증가 연산자
+		// 다른 연산 진행 후 피연산자를 증가시킴
+		
+		int num1 = 10;
+		
+		// 전위 증가부터
+		System.out.println("----- 전위 증가 연산자 -----");
+		System.out.println("초기값 : " + num1); // 10
+		System.out.println(" ++num1 1회 수행 : " + ++num1); // 11
+		System.out.println(" ++num1 2회 수행 : " + ++num1); // 12
+		System.out.println(" ++num1 3회 수행 : " + ++num1); // 13
+		System.out.println(" ++num1 4회 수행 : " + ++num1); // 14
+		System.out.println(" ++num1 5회 수행 : " + ++num1); // 15
+		System.out.println(" 최종값 : " + num1); // 15
+		
+		// 후위 증가
+		int num2 = 10;
+		
+		System.out.println("----- 후위 증가 연산자 -----");
+		System.out.println("초기값 : " + num2); // 10
+		System.out.println(" num2++ 1회 수행 : " + num2++); // 10
+		System.out.println(" num2++ 2회 수행 : " + num2++); // 11
+		System.out.println(" num2++ 3회 수행 : " + num2++); // 12
+		System.out.println(" num2++ 4회 수행 : " + num2++); // 13
+		System.out.println(" num2++ 5회 수행 : " + num2++); // 14
+		System.out.println(" 최종값 : " + num2); // 15
+	}
+	
+	public void practice1() {
+		int num = 20;
+		int result = num++ * 3 + ++num;
+		System.out.println("result : " + result); // 82
+		System.out.println("num : " + num); // 22
+	}
+}
