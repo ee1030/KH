dec813d378f425a3671a614d4680fb02c5112a52
ee1030 <sksguswoek1@gmail.com>
2020-10-15 15:36:47 +0900

    added dimensionalArray project

---
+++ 05_dimensionalArray/src/com/kh/darr/run/DarrRun.java
@@ -0,0 +1,9 @@
+package com.kh.darr.run;
+
+import com.kh.darr.service.DarrService;
+
+public class DarrRun {
+	public static void main(String[] args) {
+		DarrService das = new DarrService();
+	}
+}
---
+++ 05_dimensionalArray/src/com/kh/darr/service/DarrService.java
@@ -0,0 +1,13 @@
+package com.kh.darr.service;
+
+public class DarrService {
+
+	public void example1() {
+		// 2차원 배열
+		// - 1차원 배열의 묶음
+		
+		// 2차원 배열 선언
+		int[][] iArr;
+		
+	}
+}
