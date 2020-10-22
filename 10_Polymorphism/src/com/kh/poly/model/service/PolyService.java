e0a73fcdb010cc683f56e78c8842c56a9a1a38dc
ee1030 <sksguswoek1@gmail.com>
2020-10-22 10:34:28 +0900

    update : Polymorphism
    added binding, downCasting

--- 10_Polymorphism/src/com/kh/poly/model/service/PolyService.java
+++ 10_Polymorphism/src/com/kh/poly/model/service/PolyService.java
@@ -25,11 +25,21 @@ public class PolyService {
 		
 		// 업캐스팅 : 참조변수의 타입이 부모 타입으로 변경되는 것
 		// -> 부모타입의 참조변수로 자식 객체를 참조하는 것
-		Animal parent = new Person();
+		Animal person = new Person();
 		
 		// parent.studyJava(); // 자식객체의 고유 기능을 참조할 수 없음
 		a.eat();
-		parent.eat();
+		person.eat();
+		// 프로그램 실행 전(컴파일 단계까지) 
+		// person(Animal)이 참조하고 있는 객체는 
+		// person객체 내부에 존재하는 Animal 객체의 eat()를 호출함.
+		// --> 정적 바인딩
+		
+		// 프로그램 실행 중
+		// person(Animal)이 참조하고 있는 객체의 eat()를 호출하면
+		// 내부에 존재하는 Animal 객체의 eat()이 아닌
+		// 오버라이딩 된 Person의 eat()를 호출함
+		// --> 동적 바인딩
 		
 		Animal[] arr = new Animal[4];
 		
@@ -40,6 +50,29 @@ public class PolyService {
 		
 		for(int i = 0; i < arr.length; i++) {
 			arr[i].breath();
+			// arr의 1, 2, 3 번 인덱스 요소가 참조하고 있는
+			// 자식 객체들이 부모인 Animal로 부터 상속받은 
+			// breath() 메소드를 오버라이딩 하였기 때문에
+			// 컴파일 전에는 부모의 breath로 바인딩 되어 있다가(정적 바인딩)
+			// 실행 중에는 각자 오버라이딩 된 breath가 실행된다.(동적 바인딩)
 		}
+		
+		// 부모 참조변수로 자식만의 메소드 호출
+		// arr[1].studyJava();
+		// 부모 타입 참조변수로는 자식 내 부모 부분만 참조 가능
+		
+		// 다운 캐스팅
+		// 부모 참조 변수 = 자식 객체
+		// --> 자식 참조 변수 = 자식 객체
+		// 참조변수가 부모 -> 자식으로 변함으로써
+		// 자식객체를 온전히 참조할 수 있게 됨.
+		
+		// (Person)arr[1].studyJava(); // X
+		// -> (자료형), . 우선순위가 .이 더 높기 때문에 메소드 호출을 먼저 하게되서 실행 X
+		
+		System.out.println("\n ===== 다운 캐스팅 =====");
+		((Person)arr[1]).studyJava();
+		((Fish)arr[2]).swim();
+		((Snake)arr[3]).lock();
 	}
 }
