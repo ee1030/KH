f3c41570221c1f374aa78c842052f137c87c2fdb
ee1030 <sksguswoek1@gmail.com>
2020-10-22 14:14:37 +0900

    update : Polymorphism
    added Test class

--- 10_Polymorphism/src/com/kh/poly/ex1/model/vo/Test.java
+++ 10_Polymorphism/src/com/kh/poly/ex1/model/vo/Test.java
@@ -1,5 +1,19 @@
 package com.kh.poly.ex1.model.vo;
 
-public class Test {
-
+public abstract class Test {
+	// 미완성 메소드가 없어도 추상클래스 선언이 가능하다.
+	// -> 해당 클래스를 이용하여 객체 생성을 못하게 하고 싶을 경우
+	//	  대신 상속을 통해서는 사용 가능함
+	
+	public void method1() {
+		System.out.println("완성된 기능 1");
+	}
+	
+	public void method2() {
+		System.out.println("완성된 기능 2");
+	}
+	
+	public void method3() {
+		System.out.println("완성된 기능 3");
+	}
 }
