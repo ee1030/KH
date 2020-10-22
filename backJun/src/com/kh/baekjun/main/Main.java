96e3e1fbc0b479ebcb560378b70c9127e9ae3dae
ee1030 <sksguswoek1@gmail.com>
2020-10-22 14:18:23 +0900

    update : backJun
    4344번 문제 해결

--- backJun/src/com/kh/baekjun/main/Main.java
+++ backJun/src/com/kh/baekjun/main/Main.java
@@ -6,15 +6,37 @@ public class Main {
     public static void main(String[] args) {
     	Scanner sc = new Scanner(System.in);
     	
-    	int n = sc.nextInt();
-    	int sum = 0;
+    	int testCase = sc.nextInt();
     	
-    	for(int i = 1; i <= n; i++) {
-    		if(i%2 == 0) {
-    			sum += i;
+    	double[] avgArr = new double[testCase];
+    	
+    	for(int i = 0; i < avgArr.length; i++) {
+    		int students = sc.nextInt();
+    		
+    		double[] score = new double[students];
+    		double sum = 0;
+    		
+    		for(int j = 0; j < students; j++) {
+    			double score1 = sc.nextDouble();
+    			score[j] = score1;
+    			sum += score1;
     		}
+    		
+    		double testAvg = sum/students;
+    		int count = 0;
+    		
+    		for(int j = 0; j < score.length; j++) {
+    			if(score[j] > testAvg) {
+    				count++;
+    			}
+    		}
+    		
+    		avgArr[i] = (double)count/students * 100;
     	}
     	
-    	System.out.println(sum);
+    	for(int i = 0; i < avgArr.length; i++) {
+    		System.out.printf("%.3f", avgArr[i]);
+    		System.out.println("%");
+    	}
     }
 }
