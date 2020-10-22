14aa3215cd46dd5a116d1db2f4615c402fbeca82
ee1030 <sksguswoek1@gmail.com>
2020-10-22 12:17:32 +0900

    update : backJun
    solved codeUp problem

--- backJun/src/com/kh/baekjun/main/Main.java
+++ backJun/src/com/kh/baekjun/main/Main.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    	// น้มุ
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	int sum = 0;
+    	
+    	for(int i = 1; i <= n; i++) {
+    		if(i%2 == 0) {
+    			sum += i;
+    		}
+    	}
+    	
+    	System.out.println(sum);
     }
 }
