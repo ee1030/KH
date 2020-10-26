471fc997f6446446b575668f224ff4bb3ed3921b
ee1030 <sksguswoek1@gmail.com>
2020-10-26 19:30:42 +0900

    update : backJun
    코드업 문제풀이

--- backJun/src/com/kh/baekjun/main/Main.java
+++ backJun/src/com/kh/baekjun/main/Main.java
@@ -6,24 +6,32 @@ public class Main {
     public static void main(String[] args) {
     	Scanner sc = new Scanner(System.in);
     	
-    	int h = sc.nextInt();
-    	int r = sc.nextInt();
+    	int input = sc.nextInt();
     	
-    	for(int i = 0; i < r; i++) {
-	    	for(int j = 0; j < h; j++) {
-	    		for(int k = 0; k < j; k++) {
-	    			System.out.print(" ");
-	    		}
-	    		System.out.println("*");
-	    	}
-	    	
-	    	for(int j = h-1; j > 0; j--) {
-	    		for(int k = j; k > 1; k--) {
-	    			System.out.print(" ");
-	    		}
-	    		System.out.println("*");
-	    	}
-	    	
+    	int[] binNum = new int[1000];
+    	int i = binNum.length-1;
+    	
+    	if(input == 0) {
+    		System.out.println(0);
+    		return;
+    	}
+    	
+    	while(true) {   		
+    		binNum[i] = input%2;
+    		i--;
+    		input /= 2;
+    		if(input == 0)
+    			break;
+    	}
+    	
+    	for(int j = 0; j < binNum.length; j++) {
+    		if(binNum[j] == 1) {
+    			for(int k = j; k < binNum.length; k++) {
+    				System.out.print(binNum[k]);
+    			}
+    			break;
+    		}
+    	
     	}
     }
 }
