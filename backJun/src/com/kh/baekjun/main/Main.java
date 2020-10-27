bfd86ddaf9e3d839b38e96aafdbef8ecab8bfe28
ee1030 <sksguswoek1@gmail.com>
2020-10-27 09:17:23 +0900

    update : backJun
    코드업 1420 해결

--- backJun/src/com/kh/baekjun/main/Main.java
+++ backJun/src/com/kh/baekjun/main/Main.java
@@ -1,6 +1,5 @@
 package com.kh.baekjun.main;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -9,33 +8,5 @@ public class Main {
     	Scanner sc = new Scanner(System.in);
 
     	int input = sc.nextInt();
-    	
-    	int[] arr = new int[input];
-    	String[] strArr = new String[input];
-    	
-    	for(int i = 0; i < input; i++) {
-    		String strInput = sc.nextLine();
-    		String[] tmpArr = new String[2];
-    		
-    		tmpArr = strInput.split(" ");
-    		strArr[i] = tmpArr[0];
-    		arr[i] = Integer.parseInt(tmpArr[1]);
-    	}
-    	
-    	for(int i = 0; i < arr.length-1; i++) {
-    		for(int j = 0; j < arr.length-i-1; j++) {
-    			if(arr[j] > arr[j+1]) {
-    				int tmp = arr[j];
-    				arr[j] = arr[j+1];
-    				arr[j+1] = tmp;
-    				
-    				String strTmp = strArr[j];
-    				strArr[j] = strArr[j+1];
-    				strArr[j+1] = strTmp;
-    			}
-    		}
-    	}
-    	
-    	System.out.println(strArr[2]);
     }
 }
