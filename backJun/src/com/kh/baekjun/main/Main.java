2678de780fa26dc10c934295cf25d03a854d74a4
ee1030 <sksguswoek1@gmail.com>
2020-11-03 14:02:21 +0900

    update : backJun
    코드업 1099번 문제 풀이

--- backJun/src/com/kh/baekjun/main/Main.java
+++ backJun/src/com/kh/baekjun/main/Main.java
@@ -7,23 +7,44 @@ public class Main {
     	
     	Scanner sc = new Scanner(System.in);
     	
-    	int N = sc.nextInt();
-    	int[] nArr = new int[N];
-    	for(int i = 0; i < N; i++) {
-    		nArr[i] = sc.nextInt();
+    	int[][] maze = new int[10][10];
+    	
+    	for(int i = 0; i < maze.length; i++) {
+    		for(int j = 0; j < maze[i].length; j++) {
+    			maze[i][j] = sc.nextInt();
+    		}
     	}
     	
-    	int M = sc.nextInt();
-      	int[] mArr = new int[M];
-    	for(int i = 0; i < M; i++) {
-    		int result = 0;
-    		mArr[i] = sc.nextInt();
-    		for(int j = 0; j < nArr.length; j++) {
-    			if(nArr[j] == mArr[i]) {
-    				result = 1;
+    	int mazeCount = 1;
+    	boolean flag = false;
+    	
+    	for(int i = 1; i < maze.length; i++) {
+    		for(int j = mazeCount; j < maze[i].length; j++) {
+    			if(maze[i][j] == 1) {
+    				mazeCount = j-1;
+    				break;
+    			} else if(maze[i][j] == 2) {
+    				maze[i][j] = 9;
+    				flag = true;
+    				break;
+    			} else {
+    				maze[i][j] = 9;
     			}
     		}
-    		System.out.print(result + " ");
+    		if(flag) break;
+    	}
+    	
+    	for(int i = 0; i < maze.length; i++) {
+    		for(int j = 0; j < maze[i].length; j++) {
+    			if(i == 9 && j == 9) {
+    				System.out.print(maze[i][j]);
+    			} else {
+    				System.out.print(maze[i][j]+ " ");
+    			}
+    		}
+    		if(i != 9) {
+    			System.out.println();
+    		}
     	}
     }
 }
