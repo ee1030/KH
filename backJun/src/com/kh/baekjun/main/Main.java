93a6c0eb6c772d3a0d0b6814d01ac4b76adeef09
ee1030 <sksguswoek1@gmail.com>
2020-11-06 09:22:36 +0900

    update : backJun
    코드업 문제 풀이(문자열 마지막 문제)

--- backJun/src/com/kh/baekjun/main/Main.java
+++ backJun/src/com/kh/baekjun/main/Main.java
@@ -4,16 +4,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    	
+
     	Scanner sc = new Scanner(System.in);
     	
-    	String input = sc.nextLine();
+    	String S1 = sc.nextLine();
+    	String S2 = sc.nextLine();
+    	String S3 = sc.nextLine();
     	
-    	for(int i = 0; i < input.length(); i++) {
-    		if(input.charAt(i) == 't')
-    		System.out.print(i+1 + " ");
+    	if(S1.charAt(S1.length()-1) == S2.charAt(0) && 
+    	   S2.charAt(S2.length()-1) == S3.charAt(0) &&
+    	   S3.charAt(S3.length()-1) == S1.charAt(0)) {
+    		System.out.println("good");
+    	} else {
+    		System.out.println("bad");
     	}
-    	
-    	
     }
 }
