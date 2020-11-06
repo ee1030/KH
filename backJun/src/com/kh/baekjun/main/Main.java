98fed4d520e33dcb2b248f128500847af3402d1c
ee1030 <sksguswoek1@gmail.com>
2020-11-06 10:46:33 +0900

    update : backJun
    프로그래머스 레벨1 크레인문제 풀이중

--- backJun/src/com/kh/baekjun/main/Main.java
+++ backJun/src/com/kh/baekjun/main/Main.java
@@ -1,22 +1,33 @@
 package com.kh.baekjun.main;
 
-import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
 
-    	Scanner sc = new Scanner(System.in);
-    	
-    	String S1 = sc.nextLine();
-    	String S2 = sc.nextLine();
-    	String S3 = sc.nextLine();
-    	
-    	if(S1.charAt(S1.length()-1) == S2.charAt(0) && 
-    	   S2.charAt(S2.length()-1) == S3.charAt(0) &&
-    	   S3.charAt(S3.length()-1) == S1.charAt(0)) {
-    		System.out.println("good");
-    	} else {
-    		System.out.println("bad");
-    	}
+    }
+}
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        
+        Stack<Integer> result = new Stack<>(); 
+    
+        for(int i = 0; i < moves.length; i++) {
+        	for(int j = 0; j < board.length; j++) {
+        		if(board[j][moves[i]] != 0) {
+        			result.add(board[j][moves[i]]);
+        			if(result.size() > 1) {
+        				answer += 2;
+        				result.remove(i);
+        				result.remove(i);
+        			}
+        			board[j][moves[i]] = 0;
+        		}
+        	}
+        }
+        
+        return answer;
     }
 }
