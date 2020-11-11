68b80aeebc41886da194bc35bf000660e99c7e52
ee100 <sksguswoek1@gmail.com>
2020-11-11 12:46:58 +0900

    update : backJun
    프로그래머스 level 1 두개뽑아서 더하기 문제 풀이

--- backJun/src/com/kh/baekjun/main/Main.java
+++ backJun/src/com/kh/baekjun/main/Main.java
@@ -1,44 +1,44 @@
 package com.kh.baekjun.main;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
     	Solution sol = new Solution();
-    	
-    	int[][] board = {{0,0,1,0,0},{0,0,1,0,0},{0,2,1,0,0},{0,2,1,0,0},{0,2,1,0,0}};
-    	int[] moves = {1,2,3,3,2,3,1};
-    	System.out.println(sol.solution(board, moves));
+    	int[] numbers = {5, 0, 2, 7};
+    	System.out.println(Arrays.toString(sol.solution(numbers)));
     	
     }
 }
 
 class Solution {
-    public int solution(int[][] board, int[] moves) {
-        int answer = 0;
-        
-        List<Integer> result = new ArrayList<>(); 
-        
-        for(int i = 0; i < moves.length; i++) {
-        	for(int j = 0; j < board.length; j++) {
-        		if(board[j][moves[i]-1] != 0) {	
-        			if(!result.isEmpty()) {
-        				if(result.get(result.size()-1) == board[j][moves[i]-1]) {
-        					result.remove(result.size()-1);
-        					answer += 2;
-        				} else {
-        					result.add(board[j][moves[i]-1]);
-        				}
-        			} else {
-        				result.add(board[j][moves[i]-1]);
-        			}
-        			board[j][moves[i]-1] = 0;
-        			break;
-        		}
-        	}
-        }
-        
+    public int[] solution(int[] numbers) {
+    	
+    	List<Integer> list = new ArrayList<>();
+    	List<Integer> tmpList = new ArrayList<>();
+    	
+    	for(int i = 0; i < numbers.length-1; i++) {
+    		for(int j = i+1; j < numbers.length; j++) {
+    			list.add(numbers[i] + numbers[j]);
+    		}
+    	}
+    	
+    	for(int i : list) {
+    		if(!tmpList.contains(i)) {
+    			tmpList.add(i);
+    		}
+    	}
+    	    	
+    	int[] answer = new int[tmpList.size()];
+    	
+    	for(int i = 0; i < answer.length; i++) {
+    		answer[i] = tmpList.get(i);
+    	}
+    	
+    	Arrays.sort(answer);
+    	
         return answer;
     }
 }
